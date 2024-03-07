3c531b204e6cb710b07d9fc8e5a6ed18e12eb871
MegumiN152 <3105755134@qq.com>
2024-03-07 13:06:13 +0800

    Initial commit no2

---
+++ src/main/java/com/hh/springboot/hi.java
@@ -0,0 +1,11 @@
+package com.hh.springboot;
+
+/**
+ * @author 黄昊
+ * @version 1.0
+ **/
+public class hi {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
